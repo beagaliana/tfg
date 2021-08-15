15be8c1e5911fe61e8ac7171c9e91e63f1f3af65
Bea Galiana <bea@cambrico.net>
2021-08-15 18:04:35 +0200

    Update on LDR

--- arduino/ldr.ino
+++ arduino/ldr.ino
@@ -1,17 +1,16 @@
 
-int sensorPin = A0;   // select the input pin for ldr
-int sensorValue = 0;  // variable to store the value coming from the sensor
+int sensorPin = A0;  
+int sensorValue = 0;
 int ledPin = 13;
 
 void setup() {
-  pinMode(2, OUTPUT); //pin connected to the relay
-  Serial.begin(9600); //sets serial port for communication
+  pinMode(2, OUTPUT); 
+  Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
 }
 void loop() {
-  // read the value from the sensor:
   sensorValue = analogRead(sensorPin);
-  Serial.println(sensorValue); //prints the values coming from the sensor on the screen
+  Serial.println(sensorValue);
   if(sensorValue < 700) //setting a threshold value
     digitalWrite(2,HIGH); //turn relay ON
   else digitalWrite(2,LOW); //turn relay OFF
